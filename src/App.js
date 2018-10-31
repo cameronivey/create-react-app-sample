d9f894abe174b8c28bdf6a574515981858b87d0a
Cameron Ivey <cameron.ivey@excella.com>
2018-10-31 13:01:25 -0400

    display data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/FantasyPros_10302018_Standard.json';
 
 class App extends Component {
   render() {
+    this.dataList = data.map((player) => 
+    <tr>
+      <td>{ player["Team"] }</td>
+      <td>{ player["Overall"] }</td>
+      <td>{ player["Rank"] }</td>
+      <td>{ player["Pos"] }</td>
+    </tr>
+  );
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            { this.dataList }
           </p>
           <a
             className="App-link"
